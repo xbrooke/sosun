b09abbccde3c528ae5e817ce5b88a14aa7757f1d
xbrooke <107459849+xbrooke@users.noreply.github.com>
2025-06-08 09:34:36 +0800

    更新工作流

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-/** WARNING: DON'T EDIT THIS FILE */
-/** WARNING: DON'T EDIT THIS FILE */
-/** WARNING: DON'T EDIT THIS FILE */
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-function getPlugins() {
-  const plugins = [react(), tsconfigPaths()];
-  return plugins;
-}
-
-// 👇 修改部分：添加 base 配置（替换为你的仓库名）
 export default defineConfig({
-  base: "/sosun/",  // <-- 替换为你的仓库名，例如 /vite-ui/
-  plugins: getPlugins(),
+  base: "/sosun/",  // 仓库名一致
+  plugins: [react(), tsconfigPaths()],
 });
